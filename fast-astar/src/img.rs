bc31a4b22d771e7b36f87406582964be4fb0366c
Tobias Nienhaus <tobiasnienhaus69@gmail.com>
2021-04-01 17:23:20 +0200

    Add ability to load graphs from images

--- fast-astar/src/img.rs
+++ fast-astar/src/img.rs
@@ -1,7 +1,65 @@
-use crate::Graph;
+use crate::{Graph, Node};
+use graphlib::VertexId;
 use image::{io::Reader as ImgReader, GrayImage, Pixel};
 use std::io::Result as IoResult;
 use std::path::PathBuf;
+
+type Grid<T> = Vec<Vec<T>>;
+type BGrid = Grid<bool>;
+type IdGrid = Grid<Option<VertexId>>;
+
+pub mod traits {
+    use super::BGrid;
+    use super::Grid;
+    use super::IdGrid;
+    pub trait GridAccess<'a> {
+        type Inner;
+        fn get_2d(&'a self, pos: (usize, usize)) -> &'a Self::Inner;
+        fn get_mut_2d(&'a mut self, pos: (usize, usize)) -> &'a mut Self::Inner;
+        fn width(&self) -> usize;
+        fn height(&self) -> usize;
+    }
+
+    pub(super) trait GridInit {
+        fn init(w: usize, h: usize) -> Self;
+    }
+
+    impl<'a, T> GridAccess<'a> for Grid<T>
+    where
+        T: Clone,
+    {
+        type Inner = T;
+
+        fn get_2d(&'a self, pos: (usize, usize)) -> &'a T {
+            self.get(pos.1).unwrap().get(pos.0).unwrap()
+        }
+
+        fn get_mut_2d(&'a mut self, pos: (usize, usize)) -> &'a mut Self::Inner {
+            self.get_mut(pos.1).unwrap().get_mut(pos.0).unwrap()
+        }
+
+        fn width(&self) -> usize {
+            self.get(0).unwrap().len()
+        }
+        fn height(&self) -> usize {
+            self.len()
+        }
+    }
+
+    impl GridInit for BGrid {
+        fn init(w: usize, h: usize) -> BGrid {
+            vec![vec![false; w]; h]
+        }
+    }
+
+    impl GridInit for IdGrid {
+        fn init(w: usize, h: usize) -> IdGrid {
+            vec![vec![None; w]; h]
+        }
+    }
+}
+
+use traits::*;
 
 fn load_luma8(name: PathBuf) -> IoResult<GrayImage> {
     Ok(ImgReader::open(name)?
@@ -9,6 +67,22 @@ fn load_luma8(name: PathBuf) -> IoResult<GrayImage> {
         .decode()
         .unwrap()
         .to_luma8())
+}
+
+fn grid_from_img(img: &GrayImage, cutoff: u8) -> BGrid {
+    let mut grid = BGrid::init(img.width() as usize, img.height() as usize);
+    for (y, row) in img.rows().enumerate() {
+        for (x, pix) in row.enumerate() {
+            if pix
+                .channels()
+                .iter()
+                .fold(false, |init, rolling| init | (*rolling > cutoff))
+            {
+                *grid.get_mut_2d((x, y)) = true
+            }
+        }
+    }
+    grid
 }
 
 pub fn test_img(name: PathBuf) -> IoResult<()> {
@@ -22,4 +96,54 @@ pub fn test_img(name: PathBuf) -> IoResult<()> {
         println!();
     }
     Ok(())
+}
+
+pub fn test_grid(name: PathBuf, cutoff: u8) -> IoResult<()> {
+    let img = load_luma8(name)?;
+    let grid = grid_from_img(&img, cutoff);
+    println!("Img dim:  {}x{}", img.width(), img.height());
+    println!("Grid dim: {}x{}", grid.len(), grid.get(0).unwrap().len());
+    for row in grid {
+        for val in row {
+            print!("{}", if val { "██" } else { "  " });
+        }
+        println!();
+    }
+    Ok(())
+}
+
+pub fn load_graph_from_img(path: PathBuf) -> IoResult<(IdGrid, Graph)> {
+    load_graph_from_img_with_cutoff(path, 128)
+}
+// TODO diagonal connections
+pub fn load_graph_from_img_with_cutoff(path: PathBuf, cutoff: u8) -> IoResult<(IdGrid, Graph)> {
+    let img = load_luma8(path)?;
+    let mut graph = Graph::with_capacity(img.width() as usize * img.height() as usize);
+
+    // TODO use some sort of sparse grid maybe
+    let grid = grid_from_img(&img, cutoff);
+    let mut id_grid = IdGrid::init(img.width() as usize, img.height() as usize);
+    for (y, col) in grid.iter().enumerate() {
+        for (x, val) in col.iter().enumerate() {
+            if *val {
+                let id = Some(graph.add_vertex(Node::new(x as f64, y as f64)));
+                if let Some(id) = id {
+                    if x > 0 {
+                        if let Some(nb) = id_grid.get_2d((x - 1, y)) {
+                            graph.add_edge(&id, nb).unwrap();
+                        }
+                    }
+                    if y > 0 {
+                        if let Some(nb) = id_grid.get_2d((x, y - 1)) {
+                            graph.add_edge(&id, nb).unwrap();
+                        }
+                    }
+                }
+                *id_grid.get_mut_2d((x, y)) = id;
+            }
+        }
+    }
+
+    graph.shrink_to_fit();
+    Ok((id_grid, graph))
 }
--- fast-astar/src/main.rs
+++ fast-astar/src/main.rs
@@ -4,12 +4,53 @@ mod graph_types;
 mod img;
 use graph_types::Graph;
 use graph_types::Node;
+use img::traits::*;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+type R = Result<(), Box<dyn std::error::Error>>;
+
+fn main() -> R {
+    // dijkstra_test2()?;
+    dijkstra_test2("test_images/graph2.png")?;
+    dijkstra_test2("test_images/graph4.png")?;
+    img_test()?;
+    Ok(())
+}
+
+fn img_test() -> R {
     println!("Grayscale:");
     img::test_img("test_images/image_test.png".into())?;
     println!("RGB:");
     img::test_img("test_images/image_test2.png".into())?;
+    println!("Grayscale Grid:");
+    img::test_grid("test_images/image_test.png".into(), 128)?;
+    println!("RGB Grid:");
+    img::test_grid("test_images/image_test2.png".into(), 70)?;
+    println!("Bigger:");
+    img::test_grid("test_images/graph2.png".into(), 128)?;
+    println!("Biggest:");
+    img::test_grid("test_images/graph3.png".into(), 128)?;
+    println!("Rect:");
+    img::test_grid("test_images/graph4.png".into(), 128)?;
+    Ok(())
+}
+
+fn dijkstra_test2(name: &str) -> R {
+    let (ids, graph) = img::load_graph_from_img(name.into())?;
+    if let Some(start) = ids.get_2d((0, 0)) {
+        if let Some(end) = ids.get_2d((ids.width() - 1, ids.height() - 1)) {
+            println!("Start: {:?}", graph.fetch(start));
+            println!("End: {:?}", graph.fetch(end));
+            if let Some(p) = dijkstra::algo(&graph, start, end) {
+                println!("Path:");
+                for n in p.iter() {
+                    println!("Node: {:?}", graph.fetch(&n));
+                }
+                println!("--> Length: {}", p.len());
+            } else {
+                println!("No Path!");
+            }
+        }
+    }
     Ok(())
 }
 
@@ -39,10 +80,11 @@ fn _dijkstra_test() {
     graph.add_edge(&h, &i).unwrap();
 
     if let Some(p) = dijkstra::algo(&graph, &a, &i) {
-        println!("Path:\n");
-        for n in p {
+        println!("Path:");
+        for n in p.iter() {
             println!("Node: {:?}", graph.fetch(&n));
         }
+        println!("--> Length: {}", p.len());
     } else {
         println!("No Path!");
     }
